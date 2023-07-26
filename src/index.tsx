06734cf47baeafeb48052141d4aca67b3759f13e
Hamza <hamza.sweid963@gmail.com>
2023-07-26 11:10:15 +0400

    fix hashing

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/practical-test/index.html">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
